4b5d8d95bed581cb3f9aa02f6244b556a09f184a
Joshua Chan <JChan@ucvts.tec.nj.us>
2019-12-10 12:32:44 -0500

    2 and 6 not done

--- js/shapes.js
+++ js/shapes.js
@@ -67,16 +67,16 @@ const drawRectangle = function() {
    if (width > canvas.width || width < 1) {
      alert("Your width must be between 1 and 1024.")
    }
-   if (height > canvas.height || height < 1) {
+   else if (height > canvas.height || height < 1) {
      alert("Your height must be between 1 and 512.")
    }
-   if (x < 1 || x > 1024) {
+   else if (x < 1 || x > 1024) {
      alert("Your x-coordinate must be between 1 and 1024.")
    }
-   if (y < 1 || y > 512) {
+   else if (y < 1 || y > 512) {
      alert("Your y-coordinate must be between 1 and 512.")
    }
-   if (isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y)) {
+   else if (isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y)) {
      alert("One of your values is not a number.")
    }
  } while (width > canvas.width || width < 1 || height > canvas.height || height < 1 || x < 1 || x > 1024 || y < 1 || y > 512 || isNaN(width) || isNaN(height) || isNaN(x) || isNaN(y))
@@ -117,7 +117,53 @@ const drawColoredRectangle = function() {
  */
 
 const drawTriangle = function() {
-  
+  const canvas = document.getElementById('student-canvas-4');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  do {
+    var side1 = prompt("Side 1: ")
+    if (side1 == null) {
+      break;
+    }
+    var side2 = prompt("Side 2: ")
+    if (side2 == null) {
+      break;
+    }
+    var side3 = prompt("Side 3: ")
+    if (side3 == null) {
+      break;
+    }
+
+    var hypotenuse = Math.max(side1, side2, side3)
+    var height = Math.min (side1, side2, side3)
+    var base = Math.sqrt(hypotenuse*hypotenuse - height*height)
+
+    side1 = Number(side1)
+    side2 = Number(side2)
+    side3 = Number(side3)
+
+    if (base*base + height*height != hypotenuse*hypotenuse || base == 0 || height == 0 || hypotenuse == 0  || side1+side2+side3-hypotenuse-height != base) {
+      alert("That's not a valid right triangle.")
+  }
+    else if (isNaN(side1) || isNaN(side2) || isNaN(side3)) {
+      alert("One of your sides is not a number.")
+  }
+    else if (base > 1024 || height > 512 || hypotenuse > 1310720) {
+      alert("Your triangle won't fit on the canvas.")
+  }
+}   while ((Math.floor(base)*Math.floor(base) + height*height != hypotenuse*hypotenuse) || isNaN(side1) || isNaN(side2) || isNaN(side3) || base > 1024 || height > 512 || hypotenuse > 1310720 || base == 0 || height == 0 || hypotenuse == 0)
+
+  if ((base*base + height*height == hypotenuse*hypotenuse) && (base < 1024 && height < 512 && hypotenuse < 1145) && (base != 0 && height != 0 && hypotenuse != 0) && (base != null && height != null && hypotenuse != null)) {
+    height = height + 25
+    base = base + 25
+    ctx.beginPath();
+    ctx.moveTo(25, 25);
+    ctx.lineTo(25, height);
+    ctx.lineTo(base, height)
+    ctx.lineTo(25, 25)
+    ctx.stroke();
+}
 };
 
 /*
@@ -137,10 +183,10 @@ do {
   if (radius < 32) {
     alert("Your radius must be at least 32.")
   }
-  if (isNaN(radius)) {
+  else if (isNaN(radius)) {
     alert("Your radius is not a number.")
   }
-  if (radius > 256) {
+  else if (radius > 256) {
     alert("Your smiley face won't fit on the canvas.")
   }
 } while (radius > 256 || isNaN(radius) || radius < 32)
